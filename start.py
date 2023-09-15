0cda4b1c4882b5e7f07a916f4327a478e899a388
Ignacio Collantes <luisignacioccp@gmail.com>
2023-09-15 12:21:10 -0400

    Scrobble per day

--- start.py
+++ start.py
@@ -43,9 +43,7 @@ class Process:
         except:
             self.session = None
 
-        current_datetime = datetime.now()
-        yesterday_datetime = current_datetime - timedelta(days=1)
-        self.formatted_date = yesterday_datetime.strftime(
+        self.formatted_date = datetime.now().strftime(
             "%Y-%m-%dT%H:%M:%S.%fZ")
         self.conn = sqlite3.connect('./data.db')
         cursor = self.conn.cursor()
@@ -97,54 +95,51 @@ class Process:
             self.session = self.get_session(token)
         print("Getting history...")
         history = ytmusic.get_history()
-        total = len(history)
         i = 0
         cursor = self.conn.cursor()
-        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        last_week = (datetime.now() - timedelta(weeks=1)
+        yesterday = (datetime.now() - timedelta(days=1)
                      ).strftime('%Y-%m-%d %H:%M:%S')
         for item in history:
-            i += 1
-            if i % 50 == 0:
-                print("Scrobbling " + str(i) + " songs of " + str(total))
-            record = {
-                "artistName": item["artists"][0]["name"],
-                "trackName": item["title"],
-                "ts": self.formatted_date,
-                "albumName": item["album"]["name"] if "album" in item and item["album"] is not None else None,
-            }
-            if record["albumName"] is None:
-                record["albumName"] = record["trackName"]
-            scroble = cursor.execute(
-                'SELECT * FROM scrobbles WHERE track_name = :trackName AND artist_name = :artistName AND album_name = :albumName AND scrobbled_at BETWEEN :last_week AND :now', {
-                    "trackName": record["trackName"],
-                    "artistName": record["artistName"],
-                    "albumName": record["albumName"],
-                    "last_week": last_week,
-                    "now": now
-                }).fetchone()
-            if scroble:
-                continue
-            xml_response = lastpy.scrobble(
-                record["trackName"],
-                record["artistName"],
-                record["albumName"],
-                self.session
-            )
-            root = ET.fromstring(xml_response)
-            scrobbles = root.find('scrobbles')
-            accepted = scrobbles.get('accepted')
-            ignored = scrobbles.get('ignored')
-            if accepted == '0' and ignored == '1':
-                print("Error scrobbling " + record["trackName"] +
-                      " by " + record["artistName"] + ".")
-                print(xml_response)
-            else:
-                cursor.execute('''
-                    INSERT INTO scrobbles (track_name, artist_name, album_name, scrobbled_at)
-                    VALUES (:trackName, :artistName, :albumName, :ts)
-                ''', record)
-                self.conn.commit()
+            if item["played"] == "Today":
+                record = {
+                    "artistName": item["artists"][0]["name"],
+                    "trackName": item["title"],
+                    "ts": self.formatted_date,
+                    "albumName": item["album"]["name"] if "album" in item and item["album"] is not None else None,
+                }
+                if record["albumName"] is None:
+                    record["albumName"] = record["trackName"]
+                scroble = cursor.execute(
+                    'SELECT * FROM scrobbles WHERE track_name = :trackName AND artist_name = :artistName AND album_name = :albumName AND scrobbled_at > :yesterday', {
+                        "trackName": record["trackName"],
+                        "artistName": record["artistName"],
+                        "albumName": record["albumName"],
+                        "yesterday": yesterday
+                    }).fetchone()
+                if scroble:
+                    continue
+                xml_response = lastpy.scrobble(
+                    record["trackName"],
+                    record["artistName"],
+                    record["albumName"],
+                    self.session
+                )
+                root = ET.fromstring(xml_response)
+                scrobbles = root.find('scrobbles')
+                accepted = scrobbles.get('accepted')
+                ignored = scrobbles.get('ignored')
+                if accepted == '0' and ignored == '1':
+                    print("Error scrobbling " + record["trackName"] +
+                          " by " + record["artistName"] + ".")
+                    print(xml_response)
+                else:
+                    cursor.execute('''
+                        INSERT INTO scrobbles (track_name, artist_name, album_name, scrobbled_at)
+                        VALUES (:trackName, :artistName, :albumName, :ts)
+                    ''', record)
+                    self.conn.commit()
+                    i += 1
+        print("Scrobbled " + str(i) + " songs")
 
         cursor.close()
         self.conn.close()
